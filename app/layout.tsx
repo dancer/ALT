35540b178bb3176b2c7c07067e224a74af877516
dancer <144584931+dancer@users.noreply.github.com>
2024-09-29 00:03:10 +0100

    0

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@
 import { useEffect } from "react";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -65,6 +66,7 @@ export default function RootLayout({
             {children}
           </main>
         </DisableCopyPaste>
+        <Analytics />
       </body>
     </html>
   );
